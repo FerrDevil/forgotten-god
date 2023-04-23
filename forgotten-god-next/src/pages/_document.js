05426d0f36d767ffaafdc727f99688a817963020
Daniil <ferrdevil220@gmail.com>
2023-04-23 16:48:45 +0400

    prod changes

--- forgotten-god-next/src/pages/_document.js
+++ forgotten-god-next/src/pages/_document.js
@@ -1,13 +1,24 @@
 
 import { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components';
 
+export const getInitialProps = async () => {
+  const sheet = new ServerStyleSheet();
+  const page = renderPage((App) => (props) => 
+    sheet.collectStyles(<App {...props} />),
+  ); 
+  const styleTags = sheet.getStyleElement();
+     
+  return { ...page, styleTags };
+    
+}
 
-
-export default function Document() {
-  
+export default function Document(_, styleTags) {
   return (
       <Html lang="ru">
         <Head>
+          <link rel= "preconnect" href="https://forgotten-god.onrender.com" />
+          {styleTags}
         </Head>
         <body>
           <Main />
