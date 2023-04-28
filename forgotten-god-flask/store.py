134ab274401c5442c364118a02d3f300d8279d9f
Daniil <ferrdevil220@gmail.com>
2023-04-28 20:06:06 +0400

    payment update

--- forgotten-god-flask/store.py
+++ forgotten-god-flask/store.py
@@ -355,7 +355,7 @@ def buy_products_from_cart():
         ) for cart_item in cart
     ]
     db.session.add_all(sales)
-    db.session.remove_all(cart)
+    db.session.remove(cart)
     db.session.commit()
 
     return jsonify({"payment": "Successfull"}), 200
