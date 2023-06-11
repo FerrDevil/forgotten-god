cd4ee5aae1d2deb7e0eceda1e7d4f12bb41549d9
Daniil <ferrdevil220@gmail.com>
2023-06-12 01:52:57 +0400

    commit

--- forgotten-god-next/src/app/support/downloads/page.tsx
+++ forgotten-god-next/src/app/support/downloads/page.tsx
@@ -4,9 +4,9 @@ import BasePageLayout from "@/components/Layout/BasePageLayout"
 
 const DownloadPage = () => {
     return(
-        <BasePageLayout>
+        <>
             Этой страницы пока не существует
-        </BasePageLayout>
+        </>
     )
 }
 export default DownloadPage
