ca4b3bba2015146505007234d4ad84aa0a5993ff
Daniil <ferrdevil220@gmail.com>
2023-06-12 01:56:00 +0400

    commit

--- forgotten-god-next/src/app/user/page.tsx
+++ forgotten-god-next/src/app/user/page.tsx
@@ -6,33 +6,15 @@ import { useRouter } from "next/navigation"
 
 
 const UserPage = () => {
-    const router = useRouter()
-    const options = [
-        {
-            link: "editProfile",
-            image: '/images/thumbnail.jpg',
-            text: "Профиль",
-            component: <div>da</div>
-        },
-    ]
-    
-    const option = options.find(option => option?.link === router.query.id)
-    const currentComponent = !option ? options[0]?.component : option?.component
+   
     
 
     return(
         <>
             <UserPageWrapper>
-                <UserOptions>
-                    {options?.map((option, optionIndex) => (
-                        <UserOption href={`/user/${option.link}`} key={optionIndex}>
-                            <UserOptionImage src={option.image} alt={option.image}/>
-                            <UserOptionText>{option.text}</UserOptionText>
-                        </UserOption>
-                    ) )}
+                <UserOptions>  
                 </UserOptions>
                 <UserPanel>
-                    {currentComponent}
                 </UserPanel>
             </UserPageWrapper>
         </>
