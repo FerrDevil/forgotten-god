8b731053d154e0531b685d1827eebd2fdfff163f
Daniil <ferrdevil220@gmail.com>
2023-06-12 23:52:33 +0400

    commit

---
+++ forgotten-god-next/src/app/admin/components/AdminLayout/AdminLayout.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { useUserSelector } from "@/store/store";
+import { AdminCreateProductSVG, AdminLinks, AdminPagesLink, AdminPagesLinkDescription, AdminPanel, AdminProductsSVG, AdminSalesSVG, AdminTagsSVG, AdminUsersSVG, AdminWrapper } from "../../styles";
+
+export default function AdminLayout({children} : {children: React.ReactNode}){
+    const {userInfo} = useUserSelector()
+    if (!userInfo){
+        return <> User is not admin</>
+    }
+
+    return(
+        <AdminWrapper>
+            <AdminLinks>
+                <AdminPagesLink href="/admin/users" >
+                    <AdminUsersSVG/>
+                    <AdminPagesLinkDescription>Пользователи</AdminPagesLinkDescription>
+                </AdminPagesLink>
+                <AdminPagesLink href="/admin/sales" >
+                    <AdminSalesSVG/>
+                    <AdminPagesLinkDescription>Продажи</AdminPagesLinkDescription>
+                </AdminPagesLink>
+                <AdminPagesLink href="/admin/products" >
+                    <AdminProductsSVG/>
+                    <AdminPagesLinkDescription>Продукты</AdminPagesLinkDescription>
+                </AdminPagesLink>
+                <AdminPagesLink href="/admin/tags">
+                    <AdminTagsSVG/>
+                    <AdminPagesLinkDescription>Тэги</AdminPagesLinkDescription>
+                </AdminPagesLink>
+                <AdminPagesLink href="/admin/createProduct" >
+                    <AdminCreateProductSVG/>
+                    <AdminPagesLinkDescription>Добавить продукт</AdminPagesLinkDescription>
+                </AdminPagesLink>
+                
+            </AdminLinks>
+            <AdminPanel>
+                {children}
+            </AdminPanel>
+        </AdminWrapper>
+    )
+}
--- forgotten-god-next/src/app/admin/layout.tsx
+++ forgotten-god-next/src/app/admin/layout.tsx
@@ -2,6 +2,7 @@ import store from "@/store/store"
 import { AdminPagesLink, AdminPagesLinkDescription, AdminLinks, AdminPanel, AdminWrapper, AdminUsersSVG, AdminSalesSVG, AdminCreateProductSVG, AdminProductsSVG, AdminTagsSVG } from "./styles"
 import { Metadata } from "next"
 import { getUser } from "@/components/Layout/BasePageLayout"
+import AdminLayout from "./components/AdminLayout/AdminLayout"
 
 export const metadata: Metadata = {
     title: "Панель администратора | Forgotten God"
@@ -9,42 +10,17 @@ export const metadata: Metadata = {
 
 
 export default async function Layout({children} : {children: React.ReactNode}){
-    const userInfo = await getUser()
+    /* const userInfo = await getUser()
     if (!userInfo){
         return (
             <div>
                 User is not admin
             </div>
         )
-    }
+    } */
     return (
-        <AdminWrapper>
-            <AdminLinks>
-                <AdminPagesLink href="/admin/users" >
-                    <AdminUsersSVG/>
-                    <AdminPagesLinkDescription>Пользователи</AdminPagesLinkDescription>
-                </AdminPagesLink>
-                <AdminPagesLink href="/admin/sales" >
-                    <AdminSalesSVG/>
-                    <AdminPagesLinkDescription>Продажи</AdminPagesLinkDescription>
-                </AdminPagesLink>
-                <AdminPagesLink href="/admin/products" >
-                    <AdminProductsSVG/>
-                    <AdminPagesLinkDescription>Продукты</AdminPagesLinkDescription>
-                </AdminPagesLink>
-                <AdminPagesLink href="/admin/tags">
-                    <AdminTagsSVG/>
-                    <AdminPagesLinkDescription>Тэги</AdminPagesLinkDescription>
-                </AdminPagesLink>
-                <AdminPagesLink href="/admin/createProduct" >
-                    <AdminCreateProductSVG/>
-                    <AdminPagesLinkDescription>Добавить продукт</AdminPagesLinkDescription>
-                </AdminPagesLink>
-                
-            </AdminLinks>
-            <AdminPanel>
-                {children}
-            </AdminPanel>
-        </AdminWrapper>
+        <AdminLayout>
+            {children}
+        </AdminLayout>
     )
 }
