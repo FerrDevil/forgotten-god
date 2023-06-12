d9eecf30c34fbdb6504142f8121919d7347e075e
Daniil <ferrdevil220@gmail.com>
2023-06-12 21:45:45 +0400

    commit

--- forgotten-god-next/src/app/(auth)/login/page.tsx
+++ forgotten-god-next/src/app/(auth)/login/page.tsx
@@ -45,8 +45,8 @@ const LoginPage = () => {
     
     const formSubmit = async (event : React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         event.preventDefault()
-        try {/*  https://forgotten-god.onrender.com */
-            const data = await fetch(`${process.env.NEXT_PUBLIC_HOST_DOMAIN}/auth/login`, {
+        try {
+            const data = await fetch(`${"https://forgotten-god.onrender.com"}/auth/login`, {
                 method: 'POST',
                 body : JSON.stringify(loginInfo),
                 credentials: "include"
