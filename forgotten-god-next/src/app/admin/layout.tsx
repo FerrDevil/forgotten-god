f64de2b7919d8e4506baa609e771559f237e9bf4
Daniil <ferrdevil220@gmail.com>
2023-11-12 22:00:15 +0400

    commit

--- forgotten-god-next/src/app/admin/layout.tsx
+++ forgotten-god-next/src/app/admin/layout.tsx
@@ -1,7 +1,6 @@
 import store from "@/store/store"
 import { AdminPagesLink, AdminPagesLinkDescription, AdminLinks, AdminPanel, AdminWrapper, AdminUsersSVG, AdminSalesSVG, AdminCreateProductSVG, AdminProductsSVG, AdminTagsSVG } from "./styles"
 import { Metadata } from "next"
-import { getUser } from "@/components/Layout/BasePageLayout"
 import AdminLayout from "./components/AdminLayout/AdminLayout"
 
 export const metadata: Metadata = {
