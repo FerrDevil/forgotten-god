58404a66524f441b64b2230e6c2383305709124c
Daniil <ferrdevil220@gmail.com>
2023-11-26 00:06:14 +0400

    commit

--- forgotten-god-next/src/components/Header/Header.tsx
+++ forgotten-god-next/src/components/Header/Header.tsx
@@ -3,7 +3,38 @@ import {AdminPanelSVG, SupportLinkSVG, HeaderNavigationButton, LogoutButtonSVG, 
 import HeaderLinkItem from "./HeaderLinkItem"
 
 import LogoutButton from "./LogoutButton"
-import { getUserInfo } from "@/utils/userAuth/getUserInfo"
+import { cookies } from "next/headers";
+/* import { getUserInfo } from "@/utils/userAuth/getUserInfo" */
+
+async function getUserInfo() {
+    
+        const refreshCookie = cookies().get("refresh-fg-cookie");
+        if(!refreshCookie?.value){
+            console.log(cookies())
+            return null
+        }
+        const refreshAccess = await fetch(`${process.env.HOST_DOMAIN}/auth/refresh`, { method: "POST", credentials: "include", headers: {
+            Authorization: `Bearer ${refreshCookie.value}`
+        }})
+        /* console.log(new Headers(refreshAccess.headers) ) */
+        const response = await fetch(`${process.env.HOST_DOMAIN}/auth/getUser`, { credentials: "include", headers: {
+            cookie: refreshAccess.headers.get("set-cookie")
+        }})
+        /* const refreshCookie = cookies().get("refresh-fg-cookie")
+        const response = await fetch(`${process.env.CURRENT_DOMAIN}/api/getUser`, {method: "GET", credentials: "include", headers: {
+            cookie : `${refreshCookie.name}=${refreshCookie.value}`
+        }
+        }) */
+        
+        if (!response.ok){
+            
+          return null
+        }
+            
+        const userData = await response.json()
+        return userData
+    
+}
 
 
 const Header = async () => {
